19e2a176da11965d229ba0a8bb896a2b80a3c2e1
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-17 21:12:50 +0530

    Create highest and least frequency difference.py

---
+++ highest and least frequency difference.py
@@ -0,0 +1,13 @@
+"""
+Given an array, find the difference between highest occurrence and least occurrence of any number in an array
+
+Examples:
+
+Input  : arr[] = [7, 8, 4, 5, 4, 1, 1, 7, 7, 2, 5]
+Output : 2
+Lowest occurring element (5) occurs once.
+Highest occurring element (1 or 7) occurs 3 times
+
+Input  : arr[] = [1, 1, 1, 3, 3, 3]
+Output : 0
+"""
