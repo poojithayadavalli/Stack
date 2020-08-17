9ef05840a929ec2c4a2d462b7496a78b26d8bc96
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-17 21:07:40 +0530

    Create first repeated word.py

---
+++ first repeated word.py
@@ -0,0 +1,15 @@
+"""
+Given a string, Find the 1st repeated word in a string
+
+Examples:
+
+Input : "Ravi had been saying that he had been there"
+Output : had
+
+Input : "Ravi had been saying that"
+Output : No Repetition
+
+Input : "he had had he"
+Output : he
+
+"""
