1e6469b7b1e41317c4e5eb9be001600a558e8451
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-17 21:04:58 +0530

    Create first maxlength even string.py

---
+++ first maxlength even string.py
@@ -0,0 +1,15 @@
+"""
+Given a string of words separated by spaces. The task is to find the first maximum length even word from the string. Eg: “You are given an array of n numbers” The answer would be “an” and not “of” because “an” comes before “of”.
+
+Examples:
+
+Input:  "this is a test string"
+Output:  string
+Even length words are this, is, test, string. Even
+maximum length word is string.
+
+Input:  "geeksforgeeks is a platform for geeks"
+Output:  platform
+Only even length word is platform.
+
+"""
