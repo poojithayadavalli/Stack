43abe2cde911bc8ce38daaaaba33649a9a6c9e4c
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-20 13:06:23 +0530

    Update first repeated word.py

--- first repeated word.py
+++ first repeated word.py
@@ -1,15 +1,44 @@
 """
-Given a string, Find the 1st repeated word in a string
+Given a string, The task is to find the 1st repeated word in a string.
+
+If no repeated words in the string print "No Repetition".
+
+Input:
+First line contains the input string
+
+Output:
+Print the first repeated word
 
 Examples:
 
-Input : "Ravi had been saying that he had been there"
+Input :
+Ravi had been saying that he had been there
 Output : had
 
-Input : "Ravi had been saying that"
+Input :
+Ravi had been saying that
 Output : No Repetition
 
-Input : "he had had he"
+Input : "he had he had"
 Output : he
 
+Input:
+hello world hello
+Output:
+hello
+
+Input:
+hello
+No Repetition
 """
+x=input().split()
+a=[]
+y=None
+for i in x:
+    if i in a:
+        y=i
+        break
+    else:
+        a.append(i)
+print(y if y else "No Repetition")
+    
